2909423160e20657bbdd37083b814c83fb6addb5
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-12 11:39:02 +0330

    use css as a module

--- ep-1/src/App.js
+++ ep-1/src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
+import styles from "./App.module.css";
 
 function App() {
-
   return (
-    <div className="App">
+    <div className={styles.App}>
       <GetNameComponent name="Saeed" age={32} />
       <GetNameComponent name="Ali" age={20} />
     </div>
@@ -17,7 +16,7 @@ const GetNameComponent = (props) => {
       <h1>{props.name}</h1>
       <h2>{props.age}</h2>
     </div>
-  )
-}
+  );
+};
 
 export default App;
