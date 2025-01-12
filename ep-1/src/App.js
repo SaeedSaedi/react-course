d876cfab992b02b5ef7fc73bb8dbcacc2c0b602f
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-12 12:10:39 +0330

    use map to show list items

--- ep-1/src/App.js
+++ ep-1/src/App.js
@@ -1,27 +1,17 @@
 import './App.css';
 
 function App() {
-  const age = 21;
+  const names = ["saeed","ali","mamad"]
 
   return (
     <div className="App">
-      <GetNameComponent name="Saeed" age={32} />
-      {age > 20 ? <h1>Enter Site</h1>: <h1>Forbidden</h1>}
-      {age === 20 && <h1>Age is 20</h1>}
-
-      <button type="submit" className={age > 20 ? "success":"warning"}>Click here</button>
+      { names.map((v,i)=>{
+        return <h1 key={i}>{v}</h1>
+      })}
     </div>
   );
 }
 
-//Component
-const GetNameComponent = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-    </div>
-  )
-}
+
 
 export default App;
