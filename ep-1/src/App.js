e8d1dbb2584054d2aef50e6aa5562e45d5bdda7f
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-12 15:27:41 +0330

    course list with add,delete and complete options

--- ep-1/src/App.js
+++ ep-1/src/App.js
@@ -1,15 +1,57 @@
 import "./App.css";
 import { useState } from "react";
+import Course from "./Course";
 
 function App() {
-  const[showText,setShowText] = useState(true);
-  const toggleText = ()=>{
-    setShowText(!showText)
-  }
+  const [courseList, setCourseList] = useState([]);
+  const [newCourse, setNewCourse] = useState("");
+
+  const handleChange = (event) => {
+    setNewCourse(event.target.value);
+  };
+
+  const addCourse = () => {
+    const course = {
+      id:
+        courseList.length === 0 ? 1 : courseList[courseList.length - 1].id + 1,
+      courseName: newCourse,
+      isCompleted: false,
+    };
+    setCourseList([...courseList, course]);
+  };
+
+  const deleteCourse = (courseId) => {
+    setCourseList(courseList.filter((course) => courseId !== course.id));
+  };
+
+  const completeCourse = (courseId) => {
+    const newCourseList = courseList.map((course) => {
+      if(course.id === courseId)
+        return {...course,isCompleted: !course.isCompleted}
+      else
+        return course
+    });
+
+    setCourseList(newCourseList);
+  };
   return (
     <div className="App">
-      <button onClick={toggleText}>show/hide</button>
-      { showText && <h1>This is a header</h1>}
+      <div className="add-course">
+        <input type="text" onChange={handleChange}></input>
+        <button onClick={addCourse}>add course</button>
+      </div>
+      <div className="list">
+        {courseList.map((course, i) => {
+          return (
+            <Course
+              key={i}
+              course={course}
+              deleteCourse={deleteCourse}
+              completeCourse={completeCourse}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ ep-1/src/Course.js
@@ -0,0 +1,13 @@
+const Course = (props) => {
+  return (
+    <div style={{ backgroundColor: props.course.isCompleted ? "green":"white"}}>
+      <h1>{props.course.courseName}</h1>
+      <button onClick={() => props.deleteCourse(props.course.id)}>
+        Delete
+      </button>
+      <button onClick={() => props.completeCourse(props.course.id)}>completed</button>
+    </div>
+  );
+};
+
+export default Course;
