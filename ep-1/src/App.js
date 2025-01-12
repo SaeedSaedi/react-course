94f6ad97b5ac7ceb36ad320cd5528fa3a1a6f18e
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-12 11:15:07 +0330

    use component to return

--- ep-1/src/App.js
+++ ep-1/src/App.js
@@ -2,16 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const user = <div>
-    <h1>Saeed</h1>
-    <h2>32</h2>
-  </div>
 
   return (
     <div className="App">
-      {user}
+      <GetNameComponent />
     </div>
   );
+}
+
+//Component
+const GetNameComponent = () => {
+  return (
+    <div>
+      <h1>Saeed</h1>
+      <h2>32</h2>
+    </div>
+  )
 }
 
 export default App;
