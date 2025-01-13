911225e85c3bddecd43d2cd3a9b9fb46f14a3be8
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-13 13:12:46 +0330

    create a button for navigate using useNavigate

--- ep-4/src/Pages/About.js
+++ ep-4/src/Pages/About.js
@@ -1,5 +1,24 @@
-const About=()=>{
-    return <div>This is About page</div>
-}
+import React from "react";
+import { useNavigate } from "react-router-dom";
+const About = () => {
+  let navigate = useNavigate();
 
-export default About
+  return (
+    // <div>
+    //     <div>This is About page</div>
+    //     <button>Go to Home</button>
+    // </div>
+    <React.Fragment>
+      <div>This is About page</div>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Go to Home
+      </button>
+    </React.Fragment>
+  );
+};
+
+export default About;
