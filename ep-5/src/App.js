1ba836cb2034a53b6da467c41c3fc0c70e38fce3
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-13 16:16:23 +0330

    use quey to fetch data from api with some settings and options

--- ep-5/src/App.js
+++ ep-5/src/App.js
@@ -6,32 +6,37 @@ import Contact from "./Pages/Contact";
 import Nav from "./Pages/Nav";
 import Profile from "./Pages/Profile";
 import { useState, createContext } from "react";
-
+import { QueryClient, QueryClientProvider } from "react-query";
 export const ProfileContext = createContext();
 
 function App() {
   const [username, setUserName] = useState("saeed");
+  const client = new QueryClient({
+    defaultOptions: { queries: { refetchOnWindowFocus: false }, mutations: {} },
+  });
   return (
     <div className="App">
-      <ProfileContext.Provider value={{ username, setUserName }}>
-        <Router>
-          <div>
-            <h1>This is a header</h1>
-            <Nav />
-          </div>
+      <QueryClientProvider client={client}>
+        <ProfileContext.Provider value={{ username, setUserName }}>
+          <Router>
+            <div>
+              <h1>This is a header</h1>
+              <Nav />
+            </div>
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/profile/:name?/:id?" element={<Profile />} />
-            <Route path="*" element={<div>Not Found</div>} />
-          </Routes>
-          <div>
-            <h2>This is footer</h2>
-          </div>
-        </Router>
-      </ProfileContext.Provider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/profile/:name?/:id?" element={<Profile />} />
+              <Route path="*" element={<div>Not Found</div>} />
+            </Routes>
+            <div>
+              <h2>This is footer</h2>
+            </div>
+          </Router>
+        </ProfileContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
--- ep-5/src/Pages/Home.js
+++ ep-5/src/Pages/Home.js
@@ -1,8 +1,28 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { ProfileContext } from "../App";
+import { useQuery } from "react-query";
+import Axios from "axios";
 const Home = () => {
   const { username } = useContext(ProfileContext);
-  return <div>This is Home page - username is {username}</div>;
+  const {
+    data: catData,
+    isLoading,
+    isError,
+    error,
+    refetch,
+  } = useQuery(["cat"], () => {
+    return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+  });
+
+  if (isLoading) return <h2>Loading...</h2>;
+  if (isError) return <h2>We back soon error: {error.message}</h2>;
+  return (
+    <React.Fragment>
+      <div>This is Home page - username is {username}</div>
+      <h1>{catData?.fact}</h1>
+      <button onClick={refetch}>update</button>
+    </React.Fragment>
+  );
 };
 
 export default Home;
