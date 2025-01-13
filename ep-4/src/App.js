3d534561d588348ff602cdf9b0249492d5e3e647
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-13 13:08:14 +0330

    create separate component for nav

--- ep-4/src/App.js
+++ ep-4/src/App.js
@@ -1,26 +1,30 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
+import Nav from "./Pages/Nav";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div><h1>This is a header</h1></div>
-        <nav>
-          <Link to="/">Home</Link> |
-          <Link to="/about">About</Link> |
-          <Link to="/contact">Contact</Link> |
-        </nav>
+        <div>
+          <h1>This is a header</h1>
+          <Nav />
+        </div>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<div>Not Found</div>} />
         </Routes>
+        <div>
+          <h2>This is footer</h2>
+        </div>
       </Router>
+
     </div>
   );
 }
