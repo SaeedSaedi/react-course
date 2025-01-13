ae40c749489f9a10231c289001f91603419acaf6
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-13 11:38:49 +0330

    use fetch and axios to get data from api

--- ep-3/src/App.js
+++ ep-3/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useState } from "react"
 import './App.css';
+import Axios from "axios"
 
 function App() {
+  fetch("https://catfact.ninja/fact").then((res)=> res.json()).then((data)=>{
+    console.log(data)
+  })
+
+  Axios.get("https://catfact.ninja/fact").then((res)=>{
+    console.log(res.data)
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>fetch data</button>
+      <p></p>
     </div>
   );
 }
