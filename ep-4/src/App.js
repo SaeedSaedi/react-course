b6ff40dd9101df84fdd76fbd15f20853eb1939c9
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-13 13:03:59 +0330

    create a menu with Link

--- ep-4/src/App.js
+++ ep-4/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
@@ -8,6 +8,12 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <div><h1>This is a header</h1></div>
+        <nav>
+          <Link to="/">Home</Link> |
+          <Link to="/about">About</Link> |
+          <Link to="/contact">Contact</Link> |
+        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
