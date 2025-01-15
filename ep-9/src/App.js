eb114823820a389e7d8a3dca72462647fe85da6a
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-15 16:00:24 +0330

    create home login and contact routes and components for example

--- ep-9/src/App.js
+++ ep-9/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import Login from "./Login";
+import Contact from "./Contact";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/contact">Contact</Link>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ ep-9/src/Login.js
@@ -0,0 +1,12 @@
+const Login = () => {
+    return(
+        <div>
+            <h1>Login</h1>
+            <input />
+            <button>Login</button>
+            <button>Logout</button>
+        </div>
+    )
+};
+
+export default Login;
