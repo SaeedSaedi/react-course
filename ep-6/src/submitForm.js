5863d490fced49d2a2d77afe120ae3f5054e808b
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-15 11:45:49 +0330

    create form schema and add it to use form using yupResolver

--- ep-6/src/submitForm.js
+++ ep-6/src/submitForm.js
@@ -1,16 +1,32 @@
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 const SubmitForm = () => {
-  const { register, handleSubmit } = useForm();
+  const schema = yup.object().shape({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    age: yup.number().positive().min(18).max(100).required(),
+    password1: yup.string().min(4).max(15).required(),
+    password2: yup
+      .string()
+      .oneOf([yup.ref("password1")])
+      .required(),
+  });
+  const { register, handleSubmit } = useForm({ resolver: yupResolver(schema) });
   const onFormSubmit = (data) => {
     console.log("the form submitted");
-    console.log(data)
+    console.log(data);
   };
   return (
     <form onSubmit={handleSubmit(onFormSubmit)}>
       <input type="text" placeholder="Name..." {...register("name")} />
       <input type="text" placeholder="Email..." {...register("email")} />
       <input type="number" placeholder="Age..." {...register("age")} />
-      <input type="password" placeholder="Password..." {...register("password1")} />
+      <input
+        type="password"
+        placeholder="Password..."
+        {...register("password1")}
+      />
       <input
         type="password"
         placeholder="Confirm Password..."
