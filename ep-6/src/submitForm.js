196b03494312177e24f3d3294cacc89a8a360c16
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-15 11:27:24 +0330

    create form component

---
+++ ep-6/src/submitForm.js
@@ -0,0 +1,14 @@
+const SubmitForm = () => {
+    return(
+        <form>
+            <input type="text" placeholder="Name..." />
+            <input type="text" placeholder="Email..." />
+            <input type="number" placeholder="Age..." />
+            <input type="text" placeholder="Password..." />
+            <input type="text" placeholder="Confirm Password..." />
+            <input type="submit" />
+        </form>
+    )
+};
+
+export default SubmitForm;
