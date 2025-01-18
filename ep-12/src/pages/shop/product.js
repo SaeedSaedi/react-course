e3e7c4e6a41141ef79d41ea652acb7aaabad219c
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-18 13:15:36 +0330

    create product conditions (add,remove)

--- ep-12/src/pages/shop/product.js
+++ ep-12/src/pages/shop/product.js
@@ -1,13 +1,29 @@
+import { useContext } from "react";
+import { ShopContext } from "../../context/shopContext";
+
 const Product = (props) => {
   const { id, productName, productImage, price } = props.data;
+  const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
+  const isInCart = cartItems?.some((item) => item.id === id);
   return (
     <div className="col-3">
       <img src={productImage} className="w-100" alt=""></img>
       <h5>{productName}</h5>
       <p>price : {price}Rial</p>
-      <button className="btn btn-info btn-sm">+</button>
-      <span className="mx-1">0</span>
-      <button className="btn btn-info btn-sm">-</button>
+      <button className="btn btn-info btn-sm" onClick={() => addToCart(id)}>
+        +
+      </button>
+      <span className="mx-1">
+        {cartItems?.filter((row) => row.id === id)[0]?.count}
+      </span>
+      {isInCart && (
+        <button
+          className="btn btn-info btn-sm"
+          onClick={() => removeFromCart(id)}
+        >
+          -
+        </button>
+      )}
     </div>
   );
 };
