bedf6b830258d7e06859d3612fcb28dec63ae3fd
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-18 13:35:25 +0330

    show selected items in cart item

--- ep-12/src/pages/cart/cart.jsx
+++ ep-12/src/pages/cart/cart.jsx
@@ -1,5 +1,20 @@
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../data/products";
+import { ShopContext } from "../../context/shopContext";
+import Product from "../shop/product";
 const Cart = () => {
-  return <h1>Cart</h1>;
+  const { cartItems } = useContext(ShopContext);
+  return (
+    <React.Fragment>
+      <h1>Your cart items</h1>
+      <div className="row">
+        {PRODUCTS.map((p) => {
+          if (cartItems.some((i) => i.id === p.id && i.count > 0))
+            return <Product data={p} />;
+        })}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Cart;
